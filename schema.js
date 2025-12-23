c77e6c244b0f3f03b0c5cf425fda58a8cc54d09f
matomitw <matomi-lucky.ezekiel@teamwork.net>
2025-12-23 18:21:29 +0100

    retry

--- schema.js
+++ schema.js
@@ -1,23 +1,21 @@
-import { gql } from 'graphql-tag';
-
-const typeDefs = gql`
-
+// schema.js - Apollo Server v5 (no gql needed)
+const typeDefs = `#graphql
   type MainCard {
-      title: String!
-      image: String!
+    title: String!
+    image: String!
   }
 
   type Animal {
-      id: ID!
-      image: String!
-      title: String!
-      rating: Float
-      price: String!
-      description: [String!]!
-      slug: String!
-      stock: Int!
-      onSale: Boolean
-      category: Category
+    id: ID!
+    image: String!
+    title: String!
+    rating: Float
+    price: String!
+    description: [String!]!
+    slug: String!
+    stock: Int!
+    onSale: Boolean
+    category: Category
   }
 
   type Category {
@@ -37,20 +35,78 @@ const typeDefs = gql`
   }
 
   type Mutation {
-      addAnimal(
-        image: String!
-        title: String!
-        rating: Float
-        price: String!
-        description: [String!]!
-        slug: String!
-        stock: Int!
-        onSale: Boolean
-        category: String!
-      ): Animal
-      removeAnimal(id: ID!): Boolean!
+    addAnimal(
+      image: String!
+      title: String!
+      rating: Float
+      price: String!
+      description: [String!]!
+      slug: String!
+      stock: Int!
+      onSale: Boolean
+      category: String!
+    ): Animal
+    removeAnimal(id: ID!): Boolean!
   }
-
 `;
 
-export default typeDefs
+export default typeDefs;
+
+
+// import { gql } from '@apollo/server';
+// // import { gql } from 'graphql-tag';
+
+// const typeDefs = gql`
+
+//   type MainCard {
+//       title: String!
+//       image: String!
+//   }
+
+//   type Animal {
+//       id: ID!
+//       image: String!
+//       title: String!
+//       rating: Float
+//       price: String!
+//       description: [String!]!
+//       slug: String!
+//       stock: Int!
+//       onSale: Boolean
+//       category: Category
+//   }
+
+//   type Category {
+//     id: ID!
+//     image: String!
+//     category: String!
+//     slug: String!
+//     animals: [Animal!]!
+//   }
+
+//   type Query {
+//     mainCards: [MainCard]
+//     animals: [Animal!]!
+//     animal(slug: String!): Animal
+//     categories: [Category!]!
+//     category(slug: String!): Category
+//   }
+
+//   type Mutation {
+//       addAnimal(
+//         image: String!
+//         title: String!
+//         rating: Float
+//         price: String!
+//         description: [String!]!
+//         slug: String!
+//         stock: Int!
+//         onSale: Boolean
+//         category: String!
+//       ): Animal
+//       removeAnimal(id: ID!): Boolean!
+//   }
+
+// `;
+
+// export default typeDefs
