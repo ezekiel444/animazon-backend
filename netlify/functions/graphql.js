b6896fb5641b98c15897f17c94c31d4333cfd7d5
matomitw <matomi-lucky.ezekiel@teamwork.net>
2025-12-23 16:31:20 +0100

    netlify config

---
+++ netlify/functions/graphql.js
@@ -0,0 +1,28 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { json } from 'body-parser';
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+
+app.use(
+  '/',
+  cors(),
+  json(),
+  expressMiddleware(server),
+);
+
+// Netlify serverless function handler
+export const handler = app;
