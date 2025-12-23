7df4651e37d8191838ad5c448a3e9b62fc73a2c2
matomitw <matomi-lucky.ezekiel@teamwork.net>
2025-12-23 17:49:04 +0100

    retry

--- netlify/functions/graphql.js
+++ netlify/functions/graphql.js
@@ -1,12 +1,12 @@
 // netlify/functions/graphql.js - NETLIFY PRODUCTION ONLY
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
-import db from '../db.js';
-import typeDefs from '../schema.js';
-import Query from "../resolvers/Query.js";
-import Mutation from "../resolvers/Mutation.js";
-import Animal from "../resolvers/Animal.js";
-import Category from "../resolvers/Category.js";
+import db from '../../db.js';
+import typeDefs from '../../schema.js';
+import Query from "../../resolvers/Query.js";
+import Mutation from "../../resolvers/Mutation.js";
+import Animal from "../../resolvers/Animal.js";
+import Category from "../../resolvers/Category.js";
 
 const server = new ApolloServer({
   typeDefs,
